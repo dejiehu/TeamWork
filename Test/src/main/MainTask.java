dfb44f9305e0a9dc79b81d7eb553aaddfac102b0
dejiehu <1483513547@qq.com>
2020-10-07 11:18:28 +0800

    yioy

---
+++ Test/src/main/MainTask.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.ArrayList;
+
+public class MainTask {//成绩统计
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList arr = new ArrayList();
+		arr.add(20);
+		arr.add(67);
+		arr.add(85);
+		arr.add(95);
+		arr.add(7);
+		arr.add(332);
+		
+		//成绩排序
+		//求平均分，最大值，最小值
+		//统计区间
+	}
+	public static void Sort(ArrayList arr){
+		
+	}
+}
